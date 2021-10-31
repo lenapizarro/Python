6cfa40c8f0b67380fcaa9731b98e1079c9ac8133
Lena Pizarro <lenapizarro@Lenas-MacBook-Pro.local>
2021-10-30 23:29:01 -0600

    Adding tax.py

---
+++ PythonVSCode/tax.py
@@ -0,0 +1,24 @@
+amount = 100
+tax = 0.06
+total = amount + amount*tax
+print(total)
+
+# Converting a float to an integer and viceversa
+# amount = int(10.6)
+# amount = float(10)
+# Everything inside a simple or double quotes is considered a string
+name = 'Julia'
+print(name)
+# Double quotes can be useful if a single qupte is literally part of the string: 
+# store_name = "Julia's Store": yes
+# store_name = 'Julia's Store': will cause an error
+
+# Strings can be concatenated:
+
+hello = 'Hello, '
+greeting = hello + name
+print(greeting)
+
+# STDIO
+
+my_name = input("What's your name?")
