37377c6793f3f6400b9bf8070b02414308a763bb
Lena Pizarro <lenapizarrop@gmail.com>
2021-11-01 19:22:22 -0600

    Adding new example and modified version of weather.py

---
+++ PythonVSCode/rock_paper_scissors.py
@@ -0,0 +1,11 @@
+# --------------------------------------
+# Using conditionals and logic operators
+# ---------------------------------------
+
+computer_choice = 'scissors'
+
+user_choice = input("Do you want - rock, paper, or scissors?\n")
+
+if user_choice == computer_choice:
+    print('TIE!')
+    
--- PythonVSCode/weather.py
+++ PythonVSCode/weather.py
@@ -1,6 +1,6 @@
 #This code uses conditionals
 
-temperature = input("What is the current temperature?\n")
+temperature = int(input("What is the current temperature?\n"))
 
 if temperature > 80:
     print("It's too hot! Stay inside!")
@@ -8,3 +8,17 @@ elif temperature < 60:
     print("It's too cold, stay inside!")
 else:
     print("Enjoy the outdoors!")
+
+
+# ------------------------------------------------------------
+# Understanding other conditionals and other logical operators
+# -------------------------------------------------------------
+
+temperature2 = int(input("What is the current temperature?\n"))
+forecast = 'rain'
+
+if temperature2 > 80 or temperature2 < 60 and forecast != 'cloudy': #Using 61: first or = FALSE, and = TRUE => else will be executed 
+    print("Stay inside!")                                           #Using 40: first or = TRUE, and = TRUE => all is true, condition will be executed
+else:
+    print("Enjoy the outdoors!")
+
