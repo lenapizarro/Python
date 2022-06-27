25b201f1ddc20ce57d95888e44cf5edf6903a938
Lena Pizarro <lenapizarrop@gmail.com>
2022-06-26 19:41:45 -0600

    Adding changes to basic Hello World

--- PythonVSCode/HelloWorld.py
+++ PythonVSCode/HelloWorld.py
@@ -1,3 +1,17 @@
+#! /usr/bin/env python 3
+#Using shebang line
+
 #This code is just a validation script
 
-print('Hello World!')
+import platform
+
+def main():
+    helloWorldPrint()
+
+def helloWorldPrint():
+    print('Hello World!')
+    print('This is python version {}'.format(platform.python_version()))
+
+print('This function will print FIRST because is being called BEFORE main')
+
+if __name__ == '__main__' : main()
